75815d80cb4b3981b77395699609897391349144
Orlando Timmerman <rt582@cam.ac.uk>
2025-02-12 18:45:15 +0000

    ported get_coordinates_from_gmaps function from notebook

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import yaml
+import pandas as pd
 
 
 # function to read in yamls
@@ -19,3 +20,24 @@ def write_yaml(data, fp="unnamed.yaml"):
 def append_to_yaml(data, fp="unnamed.yaml"):
     with open(fp, "a") as file:
         yaml.dump(data, file)
+
+
+def get_coordinates_from_gmaps(location_name, gmaps_client):
+    """
+    Get the latitude and longitude of a location using the Google Maps API. Used row-wise on a DataFrame.
+
+    Args:
+        location_name (str): Location name
+        gmaps_client (googlemaps.client.Client): Google Maps API client. Requires an active, authorised Google Maps client.
+    """
+    try:
+        result = gmaps_client.geocode(location_name)
+        if result:
+            lat = result[0]["geometry"]["location"]["lat"]
+            lon = result[0]["geometry"]["location"]["lng"]
+            return pd.Series([lat, lon])
+        else:
+            return pd.Series([None, None])
+    except Exception as e:
+        print(f"Error for {location_name}: {e}")
+        return pd.Series([None, None])
