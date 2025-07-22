3242f397e4ac50cebd66547c7e8014ec3f4a5276
Orlando Timmerman <rt582@cam.ac.uk>
2025-07-22 10:01:16 +0100

    added back MEOW imported and moved png generator to utils

--- calcification/utils.py
+++ calcification/utils.py
@@ -1,28 +1,38 @@
 # general
+# custom
+import cbsyst.helpers as cbh
 import numpy as np
-from tqdm.auto import tqdm
 
 # spatial
 import xarray as xa
-
-# custom
-import cbsyst.helpers as cbh
+from tqdm.auto import tqdm
 
 
 ### cbsyst sensitivity analysis
-def create_st_ft_sensitivity_array(param_combinations: list, pertubation_percentage: float, resolution: int=20) -> xa.DataArray:
-    
+def create_st_ft_sensitivity_array(
+    param_combinations: list, pertubation_percentage: float, resolution: int = 20
+) -> xa.DataArray:
     # check if more than three parameters are passed
     if len(param_combinations[0]) != 3:
-        raise ValueError("param_combinations should be a list of tuples containing salinity, temperature, and pH_NBS values")
-    
+        raise ValueError(
+            "param_combinations should be a list of tuples containing salinity, temperature, and pH_NBS values"
+        )
+
     for Sal, Temp, pH_NBS in tqdm(param_combinations):
         ST_base = cbh.calc_ST(Sal)
         FT_base = cbh.calc_FT(Sal)
-        
+
         # define perturbation ranges for ST and FT
-        ST_values = np.linspace(ST_base-pertubation_percentage/100 * ST_base,  ST_base+pertubation_percentage/100 * ST_base, 20)
-        FT_values = np.linspace(FT_base-pertubation_percentage/100 * FT_base, FT_base+pertubation_percentage/100 * FT_base, 20)
+        ST_values = np.linspace(
+            ST_base - pertubation_percentage / 100 * ST_base,
+            ST_base + pertubation_percentage / 100 * ST_base,
+            20,
+        )
+        FT_values = np.linspace(
+            FT_base - pertubation_percentage / 100 * FT_base,
+            FT_base + pertubation_percentage / 100 * FT_base,
+            20,
+        )
 
         # initialize empty array for pH_total values
         pH_grid = np.zeros((20, 20))
@@ -30,8 +40,8 @@ def create_st_ft_sensitivity_array(param_combinations: list, pertubation_percent
         for i, ST in enumerate(ST_values):
             for j, FT in enumerate(FT_values):
                 pH_Total = cbh.pH_scale_converter(
-                    pH=pH_NBS, scale='NBS', ST=ST, FT=FT, Temp=Temp, Sal=Sal
-                ).get('pHtot', None)
+                    pH=pH_NBS, scale="NBS", ST=ST, FT=FT, Temp=Temp, Sal=Sal
+                ).get("pHtot", None)
                 pH_grid[i, j] = pH_Total
         results_dict[(Sal, Temp, pH_NBS)] = pH_grid.copy()
 
@@ -40,7 +50,15 @@ def create_st_ft_sensitivity_array(param_combinations: list, pertubation_percent
     Temp_values = sorted(set(k[1] for k in results_dict.keys()))
     pH_NBS_values = sorted(set(k[2] for k in results_dict.keys()))
     # store arrays with dimensions metadata
-    data_array = np.empty((len(Sal_values), len(Temp_values), len(pH_NBS_values), len(ST_values), len(FT_values)))
+    data_array = np.empty(
+        (
+            len(Sal_values),
+            len(Temp_values),
+            len(pH_NBS_values),
+            len(ST_values),
+            len(FT_values),
+        )
+    )
     for (v1, v2, v3), arr in results_dict.items():
         i = Sal_values.index(v1)
         j = Temp_values.index(v2)
@@ -57,23 +75,23 @@ def create_st_ft_sensitivity_array(param_combinations: list, pertubation_percent
             "ST": ST_values,
             "FT": FT_values,
         },
-        name="pH_Total"
+        name="pH_Total",
     )
-    
+
 
 def select_by_stat(ds: xa.Dataset, variables_stats: dict):
     """
     Selects values from an xarray dataset based on the specified statistics for given variables.
-    
+
     Parameters:
         ds (xr.Dataset): The input dataset with a 'param_combination' dimension and coordinates for the variables.
         variables_stats (dict): A dictionary where keys are variable names and values are the statistics to use for selection ('min', 'max', 'mean').
-    
+
     Returns:
         xr.Dataset: Dataset subset at the selected coordinate values.
     """
     selected_coords = {}
-    
+
     for var, stat in variables_stats.items():
         if stat == "min":
             selected_coords[var] = ds[var].min().item()
@@ -83,11 +101,39 @@ def select_by_stat(ds: xa.Dataset, variables_stats: dict):
             selected_coords[var] = ds[var].mean().item()
         else:
             raise ValueError(f"stat for {var} must be 'min', 'max', or 'mean'")
-    
+
     # Select the closest matching values
     ds_selected = ds.sel(selected_coords, method="nearest")
-    
+
     return ds_selected
+
+
+from pathlib import Path
+
+from PIL import Image
+
+
+def convert_png_to_jpg(directory: str) -> None:
+    """
+    Convert all PNG files in a directory to JPG format.
+
+    Args:
+        directory (str): The path to the directory containing the PNG files.
+    """
+    # fetch all current png files
+    png_files = Path(directory).glob("*.png")
+
+    for png_file in png_files:
+        with Image.open(png_file) as img:  # open file
+            # convert to RGB (PNG can have transparency)
+            rgb_img = img.convert("RGB")
+
+            # create a new filename by replacing .png with .jpg
+            jpg_file = png_file.with_suffix(".jpg")
+
+            # save image as JPG
+            rgb_img.save(jpg_file, "JPEG")
+            print(f"Converted {png_file} to {jpg_file}")
 
 
 ### DEPRECATED
@@ -117,7 +163,7 @@ def select_by_stat(ds: xa.Dataset, variables_stats: dict):
 #     # print(rate_components)
 #     num = rate_components[0]
 #     denom = rate_components[1]
-    
+
 #     # if rate_unit == 'um d-1':
 #     #     print('problem')
 #     # process numerator
@@ -127,14 +173,14 @@ def select_by_stat(ds: xa.Dataset, variables_stats: dict):
 #     else:
 #         # Initialize num_prefix as an empty string
 #         num_prefix = ""
-        
+
 #         new_rate_unit_num = num
 #         if 'mol' in num:    # moles
 #             rate_val *= MOLAR_MASS_CACO3
 #             num_prefix = num.split('mol')[0]
 #             num = num.replace('mol', 'g')
 #             new_rate_unit_num = num.replace(num_prefix, '')
-    
+
 #         elif 'g' in num:    # mass
 #             num_prefix = num.split('g')[0]
 #             # new_rate_unit_num = num.replace(num_prefix, 'g')
@@ -150,14 +196,14 @@ def select_by_stat(ds: xa.Dataset, variables_stats: dict):
 #                 new_rate_unit_num = num[1:]
 #             else:
 #                 new_rate_unit_num = num.replace(num_prefix, '')
-                    
+
 #         # check if prefix exists and is valid
 #         if num_prefix in PREFIXES:
 #             if 'm2' in num:  # area
 #                 rate_val *= PREFIXES[num_prefix]**2
 #             else:
 #                 rate_val *= PREFIXES[num_prefix]
-        
+
 #     denom_prefix = ""
 #     new_rate_unit_denom = denom
 #     # process denominator
@@ -168,7 +214,7 @@ def select_by_stat(ds: xa.Dataset, variables_stats: dict):
 #             denom_prefix = 'm'
 #         else:
 #             new_rate_unit_denom = denom.replace(denom_prefix, 'c')
-                
+
 #         if denom_prefix in PREFIXES:
 #             rate_val /= (PREFIXES[denom_prefix]/PREFIXES['c'])**2
 #     elif 'g' in denom:  # specific mass
@@ -184,49 +230,45 @@ def select_by_stat(ds: xa.Dataset, variables_stats: dict):
 #                 rate_val *= factor
 #                 new_rate_unit_denom = new_rate_unit_denom.replace(duration, 'd')
 #                 break
-            
+
 #     new_rate_unit = f"{new_rate_unit_num} {new_rate_unit_denom}"
 
-#     return rate_val, new_rate_unit   
+#     return rate_val, new_rate_unit
 
-    
-    
-    
-    # def uniquify_dois(df):
+
+# def uniquify_dois(df):
 #     """
 #     Uniquify dois to reflect locations (for studies with multiple locations)
-    
+
 #     Args:
 #         df (pd.DataFrame): dataframe containing doi and location columns
-    
+
 #     Returns:
 #         pd.DataFrame: dataframe with uniquified dois, and copies of original
 #     """
 #     df['original_doi'] = df['doi']
 #     temp_df = df.copy()
 #     temp_df['location_lower'] = temp_df['location'].str.lower()
-    
-    
-    
+
+
 #     unique_locs = temp_df.drop_duplicates(['location_lower', 'coords', 'cleaned_coords'])[['doi', 'location']]
 #     # unique_locs.dropna(subset=['latitude', 'longitude'], inplace=True) # remove empty rows
 #     dois = unique_locs.doi
 #     temp_df.index = uniquify_repeated_values(dois)
-#     doi_location_map = dict(zip(zip(temp_df.drop_duplicates(subset=['doi', 'location_lower', 'coords', 'cleaned_coords'])['doi'], 
-#                                 temp_df.drop_duplicates(subset=['doi', 'location_lower', 'coords', 'cleaned_coords'])['location']), 
+#     doi_location_map = dict(zip(zip(temp_df.drop_duplicates(subset=['doi', 'location_lower', 'coords', 'cleaned_coords'])['doi'],
+#                                 temp_df.drop_duplicates(subset=['doi', 'location_lower', 'coords', 'cleaned_coords'])['location']),
 #                             dois))
 #     temp_df['doi'] = [doi_location_map.get((doi, loc), doi) for doi, loc in zip(temp_df['doi'], temp_df['location'])]
 #     # temp_df['doi'] = temp_df.index
 #     return temp_df
 
-    
-    
-    # unique_locs.doi = utils.uniquify_repeated_values(df.drop_duplicates(subset=['doi', 'location']).doi)
 
-    # # create a dictionary mapping from original (doi, location) pairs to uniquified dois
-    # doi_location_map = dict(zip(zip(df.drop_duplicates(subset=['doi', 'location'])['doi'], 
-    #                             df.drop_duplicates(subset=['doi', 'location'])['location']), 
-    #                         unique_locs['doi']))
-    # df['doi'] = [doi_location_map.get((doi, loc), doi) for doi, loc in zip(df['doi'], df['location'])]
-    
-    # return df
+# unique_locs.doi = utils.uniquify_repeated_values(df.drop_duplicates(subset=['doi', 'location']).doi)
+
+# # create a dictionary mapping from original (doi, location) pairs to uniquified dois
+# doi_location_map = dict(zip(zip(df.drop_duplicates(subset=['doi', 'location'])['doi'],
+#                             df.drop_duplicates(subset=['doi', 'location'])['location']),
+#                         unique_locs['doi']))
+# df['doi'] = [doi_location_map.get((doi, loc), doi) for doi, loc in zip(df['doi'], df['location'])]
+
+# return df
