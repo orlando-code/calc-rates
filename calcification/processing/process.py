3bea77dc79c5b749cae7723e38102df7ffbef6b1
Orlando Timmerman <rt582@cam.ac.uk>
2025-07-29 16:35:57 +0100

    extended climatology for process_climatology_data

--- calcification/processing/process.py
+++ calcification/processing/process.py
@@ -4,13 +4,17 @@ from typing import Optional
 import pandas as pd
 
 from calcification.analysis import analysis
-from calcification.processing import carbonate_chemistry, climatology, treatment_groups
+from calcification.processing import (
+    carbonate_processing,
+    climatology,
+    groups_processing,
+)
 from calcification.utils import config
 
 logging.basicConfig(level=logging.INFO)
 
 
-def process_calcification_data(
+def process_extracted_calcification_data(
     fp: str, sheet_name: str = "all_data", selection_dict: Optional[dict] = None
 ) -> pd.DataFrame:
     """
@@ -27,41 +31,41 @@ def process_calcification_data(
     if selection_dict is None:  # exclude selected rows
         selection_dict = {"include": "yes"}
 
-    logging.info("Populating carbonate chemistry...")
-    carbonate_df = carbonate_chemistry.populate_carbonate_chemistry(
+    # logging.info("Populating carbonate chemistry...")
+    carbonate_df = carbonate_processing.populate_carbonate_chemistry(
         fp, sheet_name=sheet_name, selection_dict=selection_dict
     )
-
-    logging.info("Assigning treatment groups...")
-    carbonate_df = treatment_groups.assign_treatment_groups_multilevel(carbonate_df)
+    # logging.info("Assigning treatment groups...")
+    carbonate_df = groups_processing.assign_treatment_groups_multilevel(carbonate_df)
 
     logging.info("Aggregating treatments with individual samples...")
-    carbonate_df = treatment_groups.aggregate_treatments_with_individual_samples(
+    carbonate_df = groups_processing.aggregate_treatments_rows_with_individual_samples(
         carbonate_df
     )
 
-    logging.info("Calculating effect sizes...")
-    carbonate_df = analysis.calculate_effect_for_df(carbonate_df.reset_index(drop=True))
+    # logging.info("Calculating effect sizes...")
+    carbonate_df = analysis.calculate_effect_for_df(carbonate_df)
 
     return carbonate_df
 
 
 def process_climatology_data(
-    df: pd.DataFrame,
+    experimental_df: pd.DataFrame,
     ph_clim_path: Optional[str] = None,
     sst_clim_path: Optional[str] = None,
     locations_path: Optional[str] = None,
-) -> pd.DataFrame:
+) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Merge processed data with climatology and compute global average anomalies.
 
     Args:
-        df (pd.DataFrame): Processed calcification DataFrame.
+        experimental_df (pd.DataFrame): Processed calcification DataFrame (with latitudes and longitudes).
         ph_clim_path (str): Path to pH climatology CSV.
         sst_clim_path (str): Path to SST climatology CSV.
         locations_path (str): Path to locations YAML.
 
     Returns:
+        pd.DataFrame: DataFrame with local anomalies by scenario and time_frame (2030, 2050, 2090).
         pd.DataFrame: DataFrame with global average anomalies by scenario and time_frame.
     """
     ph_clim_path = ph_clim_path or (
@@ -85,24 +89,55 @@ def process_climatology_data(
     merged_clim_df_mi = merged_clim_df.set_index(
         ["doi", "location", "longitude", "latitude"]
     )
-    df_mi = df.set_index(["doi", "location", "longitude", "latitude"])
-    clim_df = df_mi.join(merged_clim_df_mi, how="inner")
+    experimental_df_mi = experimental_df.set_index(
+        ["doi", "location", "longitude", "latitude"]
+    )
+    local_climatology_df = experimental_df_mi.join(merged_clim_df_mi, how="inner")
 
     logging.info(
         f"Unique locations in climatology: {len(merged_clim_df_mi.index.unique())}, "
-        f"locations in working dataframe: {len(df.drop_duplicates('doi', keep='first'))}"
+        f"locations in working experimental dataframe: {len(experimental_df.drop_duplicates('doi', keep='first'))}"
     )
 
     # exclude aquaria locations # TODO: make this more robust/automated
-    clim_df = clim_df[
-        ~clim_df.index.get_level_values("location").str.contains(
+    local_climatology_df = local_climatology_df[
+        ~local_climatology_df.index.get_level_values("location").str.contains(
             "monaco|portugal|uk", case=False, na=False
         )
     ]
 
+    ph_anomalies = climatology.generate_location_specific_climatology_anomalies(
+        local_climatology_df, "ph"
+    )
+    sst_anomalies = climatology.generate_location_specific_climatology_anomalies(
+        local_climatology_df, "sst"
+    )
+    # Merge the two DataFrames on the relevant columns
+    merged_anomalies = pd.merge(
+        ph_anomalies,
+        sst_anomalies,
+        on=[
+            "doi",
+            "location",
+            "longitude",
+            "latitude",
+            "scenario",
+            "time_frame",
+            "percentile",
+        ],
+        suffixes=("_ph", "_sst"),
+    ).drop(columns=["scenario_var_ph", "scenario_var_sst"])
+
+    global_anomaly_df = (
+        merged_anomalies.groupby(["scenario", "time_frame", "percentile"])[
+            ["anomaly_value_ph", "anomaly_value_sst"]
+        ]
+        .mean()
+        .reset_index()
+    )  # average spatially
     # calculate global average anomalies for dataframe
-    result = (
-        clim_df.reset_index()
+    global_future_anomaly_df = (
+        local_climatology_df.reset_index()
         .groupby(["scenario", "time_frame"])
         .agg(
             mean_sst_anomaly=("mean_sst_20y_anomaly_ensemble", "mean"),
@@ -110,4 +145,5 @@ def process_climatology_data(
         )
         .reset_index()
     )
-    return result
+    # return local_climatology_df, future_climatology_df
+    return local_climatology_df, global_future_anomaly_df, global_anomaly_df
