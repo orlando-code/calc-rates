faa877ec36adb9f746ac5fb243d18f0e54519768
Orlando Timmerman <rt582@cam.ac.uk>
2025-07-29 16:28:39 +0100

    moved functions to deprecated and added annotate_axes_with_letters

--- calcification/plotting/plot_utils.py
+++ calcification/plotting/plot_utils.py
@@ -1,17 +1,13 @@
 # general
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 # spatial
 # R
-import seaborn as sns
 from scipy import interpolate
 
-from calcification.plotting import plot_config
 from calcification.utils import config, utils
 
 
@@ -34,6 +30,22 @@ def save_fig(
     print(f"Figure saved to {fig_fp}")
 
 
+def annotate_axes_with_letters(
+    axes: plt.Axes, fontsize: int = 16, xy: tuple[float, float] = (0.97, 0.05)
+) -> plt.Axes:
+    """Annotate successive axes with letters for publication-ready plots."""
+    for i, ax in enumerate(axes.flatten()):
+        # annotate with letter
+        ax.annotate(
+            chr(65 + i),  # from A to Z
+            xy=xy,
+            xycoords="axes fraction",
+            fontsize=fontsize,
+            ha="right",
+            va="center",
+        )
+
+
 def interpolate_spline(
     xs: np.ndarray, ys: np.ndarray, npoints: int = 100, k: int = 2
 ) -> tuple[np.ndarray, np.ndarray]:
@@ -52,68 +64,6 @@ def interpolate_spline(
     spline = interpolate.splrep(xs, ys, k=k)
     smooth = interpolate.splev(x_fine, spline)
     return x_fine, smooth
-
-
-def set_up_regression_plot(var: str):
-    if var == "phtot":
-        xlab = "$\\Delta$ pH"
-        xlim = (-1, 0.1)
-        predlim = xlim
-        scenario_var = "ph"
-    elif var == "temp":
-        xlab = "$\\Delta$ Temperature ($^\\circ C$)"
-        xlim = (-1, 10)
-        predlim = xlim
-        scenario_var = "sst"
-    return xlab, xlim, predlim, scenario_var
-
-
-def add_climatology_lines_to_plot(
-    ax: matplotlib.axes.Axes,
-    future_global_anomaly_df: pd.DataFrame,
-    scenario_var: str,
-    xlim: tuple[float, float],
-) -> matplotlib.axes.Axes:
-    """
-    Add climatology lines to the plot for different scenarios.
-
-    Args:
-        ax (matplotlib.axes.Axes): The axis to add the lines to.
-        future_global_anomaly_df (pd.DataFrame): DataFrame containing future climate scenario data. Must have scenario, time_frame, and mean_{scenario_var}_anomaly columns.
-        scenario_var (str): Variable name in future_global_anomaly_df to use for reference lines.
-        xlim (tuple[float, float]): x-axis limits for the plot.
-
-    Returns:
-        matplotlib.axes.Axes: The axis with the added lines.
-    """
-    scenarios = future_global_anomaly_df["scenario"].unique()
-    scenario_colours = sns.color_palette("Reds", len(scenarios))
-    scenario_colour_dict = {
-        scenario: scenario_colours[i] for i, scenario in enumerate(scenarios)
-    }
-    original_ylim = ax.get_ylim()  # get the original y-axis limits
-    scenario_lines = []
-    for scenario in scenarios:
-        # add climatology lines to the plot
-        predicted_effect_sizes = future_global_anomaly_df[
-            (future_global_anomaly_df["time_frame"] == 2090)
-            & (future_global_anomaly_df["scenario"] == scenario)
-        ][:][f"mean_{scenario_var}_anomaly"]
-
-        # plot vertical lines for each predicted effect size
-        for effect_size in predicted_effect_sizes:
-            line = ax.vlines(
-                x=effect_size,
-                ymin=original_ylim[0],
-                ymax=original_ylim[1],
-                color=scenario_colour_dict[scenario],
-                linestyle="--",
-                label=plot_config.SCENARIO_MAP[scenario],
-                zorder=5,
-            )
-            scenario_lines.append(line)
-    ax.set_ylim(original_ylim[0], original_ylim[1])  # crop to y lim
-    return ax
 
 
 class Polynomial:
@@ -167,3 +117,67 @@ def format_geo_axes(
     )
 
     return ax
+
+
+# --- Deprecated ---
+
+# def set_up_regression_plot(var: str):
+#     if var == "phtot":
+#         xlab = "$\\Delta$ pH"
+#         xlim = (-1, 0.1)
+#         predlim = xlim
+#         scenario_var = "ph"
+#     elif var == "temp":
+#         xlab = "$\\Delta$ Temperature ($^\\circ C$)"
+#         xlim = (-1, 10)
+#         predlim = xlim
+#         scenario_var = "sst"
+#     return xlab, xlim, predlim, scenario_var
+
+
+# def add_climatology_lines_to_plot(
+#     ax: matplotlib.axes.Axes,
+#     future_global_anomaly_df: pd.DataFrame,
+#     scenario_var: str,
+#     xlim: tuple[float, float],
+# ) -> matplotlib.axes.Axes:
+#     """
+#     Add climatology lines to the plot for different scenarios.
+
+#     Args:
+#         ax (matplotlib.axes.Axes): The axis to add the lines to.
+#         future_global_anomaly_df (pd.DataFrame): DataFrame containing future climate scenario data. Must have scenario, time_frame, and mean_{scenario_var}_anomaly columns.
+#         scenario_var (str): Variable name in future_global_anomaly_df to use for reference lines.
+#         xlim (tuple[float, float]): x-axis limits for the plot.
+
+#     Returns:
+#         matplotlib.axes.Axes: The axis with the added lines.
+#     """
+#     scenarios = future_global_anomaly_df["scenario"].unique()
+#     scenario_colours = sns.color_palette("Reds", len(scenarios))
+#     scenario_colour_dict = {
+#         scenario: scenario_colours[i] for i, scenario in enumerate(scenarios)
+#     }
+#     original_ylim = ax.get_ylim()  # get the original y-axis limits
+#     scenario_lines = []
+#     for scenario in scenarios:
+#         # add climatology lines to the plot
+#         predicted_effect_sizes = future_global_anomaly_df[
+#             (future_global_anomaly_df["time_frame"] == 2090)
+#             & (future_global_anomaly_df["scenario"] == scenario)
+#         ][:][f"mean_{scenario_var}_anomaly"]
+
+#         # plot vertical lines for each predicted effect size
+#         for effect_size in predicted_effect_sizes:
+#             line = ax.vlines(
+#                 x=effect_size,
+#                 ymin=original_ylim[0],
+#                 ymax=original_ylim[1],
+#                 color=scenario_colour_dict[scenario],
+#                 linestyle="--",
+#                 label=plot_config.SCENARIO_MAP[scenario],
+#                 zorder=5,
+#             )
+#             scenario_lines.append(line)
+#     ax.set_ylim(original_ylim[0], original_ylim[1])  # crop to y lim
+#     return ax
